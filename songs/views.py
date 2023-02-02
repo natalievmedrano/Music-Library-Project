40c8388ca90fc1c34e5b94df00dea7b803fbff98
natalievmedrano <nataliemed0403@gmail.com>
2023-02-02 14:06:43 -0600

    created functionality to update a specific song in the Songs app view file

--- songs/views.py
+++ songs/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,12 +20,16 @@ def songs_list(request):
         serializer.save()
         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def song_detail(request,pk):
- try:
-    song = Songs.objects.get(pk=pk)
-    serializer = SongSerializer(song)
-    return Response(serializer.data)
- except Songs.DoesNotExist:
-    return Response(status= status.HTTP_404_NOT_FOUND)
+    song= get_object_or_404(Songs, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
    

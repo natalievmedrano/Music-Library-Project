ac08767c1e8535e2d04e1a64aad6568730cbda3e
natalievmedrano <nataliemed0403@gmail.com>
2023-02-02 13:19:46 -0600

    registered songs model with admin site + completed post functionality for Songs

--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,20 @@
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Songs
 
-@api_view(['GET'])
+
+@api_view(['GET','POST'])
 def songs_list(request):
 
-    return Response('ok')
-
+    if request.method == 'GET':
+        songs = Songs.objects.all()
+        serializer= SongSerializer(songs , many= True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer=SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
